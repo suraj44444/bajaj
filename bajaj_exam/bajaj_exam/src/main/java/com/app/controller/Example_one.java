4303c20317eb94b53ab1bcda1e09d6b05c6b0eaf
suraj44444 <73593489+suraj44444@users.noreply.github.com>
2022-04-28 21:16:03 +0530

    Add files via upload

---
+++ bajaj_exam/bajaj_exam/src/main/java/com/app/controller/Example_one.java
@@ -0,0 +1,47 @@
+package com.app.controller;
+
+import java.util.Arrays;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.dto.Request;
+import com.app.dto.Response;
+
+@RestController
+@RequestMapping
+public class Example_one {
+	//"data": [“A”,”1”,”334”,”4”,”R”]
+	@PostMapping("/example")
+	public Response example1(@RequestBody Request req) {
+		Response response=new Response();
+		System.out.println(Arrays.toString(req.getAlphabets()));
+		//
+		int place=0,place2=0;
+		String[] obj=req.getAlphabets();
+		String[]  alphabets=new String[obj.length];
+		int[] num=new int[obj.length];
+		
+		for(int i=0;i<obj.length;i++) {
+			//System.out.println(obj[i]);
+			if(Character.isLetter(obj[i].charAt(0))) {	
+				//System.out.println(obj[i].charAt(0));
+				alphabets[place++]=obj[i];
+			}else {
+				num[place2++]=Integer.parseInt(obj[i]);
+			}
+		}
+		
+		response.set_success(true);
+		response.setAlphabets(alphabets);
+		response.setNumbers(num);
+		response.setEmail("sp40070@gmail.com");
+		response.setRoll_number("PD1299");
+		response.setUser_id("Suraj_Anna_Pawar_210941220201");
+
+		return response;
+	}
+
+}
